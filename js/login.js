91e9e7dcee1dc20a818d5301543a69924b2db983
sirsuccess <kanuamani@gmail.com>
2019-06-18 14:07:08 +0100

    login js created

---
+++ js/login.js
@@ -0,0 +1,42 @@
+$(document).ready(() => {
+    var submit = $("#submit");
+  
+    submit.click(function (e) {
+      e.preventDefault()
+      var userName = $("#username").val();
+      var pass = $("#password").val();
+      //console.log(pass)
+  
+      $.ajax({
+        url: "http://localhost:3000/users",
+        type: 'GET',
+        error: function () {
+          alert('error')
+        },
+        success: function (data) {
+          console.log('done')
+          data.map(user => {
+            // console.log(user.username)
+            if (user.username === userName && user.password == pass) {
+              if (user.roleID == 1) {
+                sessionStorage.setItem("user", user.id);
+                return window.location.href = "./staff/home.html";
+                
+              }else{
+                sessionStorage.setItem("user", user.id);
+                return window.location.href = "./managemnt/home.html";
+                
+              }
+            }
+            return
+        })
+        let warn = $(".bg-warning")
+        warn.append("Opps..! incorrect credentials, try again")
+        // warn.remove();
+        }
+    }
+    );
+  
+  
+    })
+  })
